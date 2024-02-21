96feaa44abc767dd3ab6c02097d9e7b6ef78850a
Ottavia <ottavia.bertolli.13@ucl.ac.uk>
2024-02-21 10:51:39 +0000

    updated the get_observation function with predefined actions, created the test file

---
+++ scripts/test_prime_climb.sh
@@ -0,0 +1,6 @@
+python api/play_game.py \
+    --agent_1_path agents.gpt.GPT4Text \
+    --agent_2_path agents.gpt.GPT4Text \
+    --game_path games.prime_climb.prime_climb.PrimeClimbGame \
+    --show_state \
+    --num_matches 50
